ea6646d6f8a326e0dfeb68b7c5b1592aca8605a5
franciscovillaescusa <villaescusa.francisco@gmail.com>
2022-08-20 13:30:59 -0400

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
@@ -32,17 +32,11 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx_rtd_theme",
+    "sphinx_tabs.tabs",
     "nbsphinx",
     "sphinx_gallery.load_style",
-    #"sphinx_gallery.gen_gallery",
-    "sphinx_tabs.tabs",
+    "sphinx_rtd_theme",
 ]
-
-sphinx_gallery_conf = {
-     'examples_dirs': '../examples',   # path to your example scripts
-     'gallery_dirs': 'auto_examples',  # path to where to save gallery generated output
-}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -51,6 +45,8 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
+
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
--- docs/source/tutorials.rst
+++ docs/source/tutorials.rst
@@ -1,7 +1,12 @@
 Tutorials
 =========
 
+We provide multiple tutorials to help the user to read and manipulate CAMELS data.
+
+All the jupyter notebooks can be downloaded locally, uploaded to the `CAMELS Binder <https://binder.flatironinstitute.org/~sgenel/CAMELS_PUBLIC>`_, and executed there.
+
 .. nbgallery::
    
    examples/Reading & Manipulating Snapshots
    examples/Pk
+
